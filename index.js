9beac9e71651122c5d306453354e5f4a8497f616
Neelam Panjwani <106943860+neelampanj@users.noreply.github.com>
2023-03-01 12:28:32 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-import images from './images';
-import meal from '../assets/meal.mp4';
-import data from './data';
+import AboutUs from './AboutUs/AboutUs';
+import Chef from './Chef/Chef';
+import FindUs from './Findus/FindUs';
+import Footer from './Footer/Footer';
+import Gallery from './Gallery/Gallery';
+import Header from './Header/Header';
+import Intro from './Intro/Intro';
+import Laurels from './Laurels/Laurels';
+import SpecialMenu from './Menu/SpecialMenu';
 
-export { images, meal, data };
+export {
+  AboutUs,
+  Chef,
+  FindUs,
+  Footer,
+  Gallery,
+  Header,
+  Intro,
+  Laurels,
+  SpecialMenu,
+};
